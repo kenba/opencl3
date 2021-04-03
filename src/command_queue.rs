157b10ebf1d8eaf4c6e5b955007b923824fa9875
Ken Barker <ken.barker@via-technology.aero>
2021-04-03 16:18:00 +0100

    Remove `Clone` to revert PR #4

--- src/command_queue.rs
+++ src/command_queue.rs
@@ -35,13 +35,6 @@ pub struct CommandQueue {
     max_work_item_dimensions: cl_uint,
 }
 
-impl Clone for CommandQueue {
-    fn clone(&self) -> CommandQueue {
-        retain_command_queue(self.queue).expect("Error: clRetainCommandQueue");
-        CommandQueue::new(self.queue, self.max_work_item_dimensions)
-    }
-}
-
 impl Drop for CommandQueue {
     fn drop(&mut self) {
         release_command_queue(self.queue).expect("Error: clReleaseCommandQueue")
